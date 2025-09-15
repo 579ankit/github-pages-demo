ce4746871af23731bce1c8d6cf70709fa2d04971
Ankit <jindalankit321@gmail.com>
2025-09-15 11:05:40 +0530

    Updated some code

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Ankit Agrawal!!
+          Hello Ankit!
         </a>
       </header>
     </div>
